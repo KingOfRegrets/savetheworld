968683b5ea0e9ef84579908d1c3d7e75a003a74d
ze jun <ZeJun@ze-juns-mbp.lan>
2017-06-06 16:40:17 -0400

    Initial Commit

---
+++ savetheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savetheworld
+//
+//  Created by Ze Jun Liu on 2017-06-06.
+//  Copyright © 2017 Ze Jun Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
